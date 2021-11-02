f8dc722052a41f6b0f21641ea36ac5f8a034844e
rizqitsani <rizqitsani@gmail.com>
2021-11-02 17:24:39 +0700

    feat: change CustomLink color

--- components/CustomLink.jsx
+++ components/CustomLink.jsx
@@ -12,7 +12,7 @@ export default function CustomLink({
       {...rest}
       openNewTab={openNewTab}
       className={clsx(
-        'inline-flex items-center font-bold hover:text-primary-400 animated-underline',
+        'inline-flex items-center font-bold hover:text-teal-400 animated-underline',
         className
       )}
     >
