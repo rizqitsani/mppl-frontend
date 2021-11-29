4dc5ba1f38c28b6810b91590ed94f333114d3b5e
rizqitsani <rizqitsani@gmail.com>
2021-11-29 19:21:55 +0700

    feat: get product detail from api

--- src/types/api.ts
+++ src/types/api.ts
@@ -10,10 +10,16 @@ export interface ProductApi {
   data: Product[];
 }
 
+export interface ProductDetailApi {
+  message: string;
+  data: Product;
+}
+
 export interface Product {
   id: string;
   name: string;
   price: number;
+  description: string;
   stock: number;
   available: false;
   created_at: string;
