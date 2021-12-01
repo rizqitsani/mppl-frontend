c4dc2233f0dc41f6f7f471fb6feceb7c6b2d703c
rizqitsani <rizqitsani@gmail.com>
2021-12-01 10:25:29 +0700

    feat: change ButtonLink style

--- src/components/links/ButtonLink.tsx
+++ src/components/links/ButtonLink.tsx
@@ -26,17 +26,17 @@ export default function ButtonLink({
     <UnstyledLink
       {...rest}
       className={clsx(
-        'py-2 px-4 flex items-center justify-center rounded font-medium text-sm hover:text-teal-400 animated-underline',
-        'border border-gray-600 shadow-sm',
+        'py-2 px-4 rounded text-sm hover:text-teal-400 font-medium',
+        'border shadow-sm',
         'focus:outline-none focus-visible:text-teal-400',
-        {
-          'bg-dark text-white': variant === 'dark',
-          'bg-white text-dark hover:bg-gray-100 hover:text-dark focus-visible:text-dark border-gray-400':
-            variant === 'light',
-          'bg-teal-400 text-black hover:bg-teal-400/90 hover:text-black border-teal-500 focus-visible:text-dark':
-            variant === 'primary',
-        },
-        isFullWidth && 'w-full',
+        variant === 'primary'
+          ? 'bg-teal-400 text-white hover:bg-teal-400/90 hover:text-white border-teal-500 disabled:hover:bg-teal-400 disabled:brightness-75  focus-visible:text-dark'
+          : variant === 'light'
+          ? 'bg-white disabled:bg-gray-200 text-dark hover:bg-gray-200 hover:text-dark focus-visible:text-dark border-gray-400 disabled:hover:text-dark'
+          : variant === 'dark'
+          ? 'bg-dark disabled:bg-gray-700 text-white disabled:hover:text-white'
+          : 'border-gray-600',
+        isFullWidth && 'flex justify-center w-full',
         className
       )}
     >
